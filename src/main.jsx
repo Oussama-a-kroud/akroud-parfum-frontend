4a0518f1c9ca1a5d9a26f26642ea399e1efff036
Oussama Akroud <oussamaakroud82@gmail.com>
2026-07-14 00:59:30 +0100

    fix: add axios base url

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios' // <--- زدت هاد السطر باش نعيطو لـ axios
 import './index.css'
 import App from './App.jsx'
+
+// <--- زدت هاد السطر باش ياخد الرابط ديال الباكاند من DigitalOcean
+axios.defaults.baseURL = import.meta.env.VITE_API_URL; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
